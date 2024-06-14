6331bdfc20300a376069d5b437b5e5106a7a61aa
anand <anandbukka01@gmail.com>
2024-06-14 13:56:50 +0530

    new test added

---
+++ src/test/java/com/learn/automation/testcases/SampleTest.java
@@ -0,0 +1,21 @@
+package com.learn.automation.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.learn.automation.constants.constants;
+import com.learn.automation.dataprovider.customDataProvider;
+import com.learn.automation.helper.BaseClass;
+import com.learn.automation.pages.Loginpage;
+
+
+
+public class SampleTest extends BaseClass {
+	
+//sample test is being written	
+
+}
